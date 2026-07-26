92ea4d59f85f17da3ad3c5a1ee86f4a1b83ad11b
Twisha Patel <twipatel2006@gmail.com>
2026-07-26 12:54:41 +0530

    Update

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,7 +1,7 @@
 import User from "../models/User.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import { generateAccessToken } from "../utils/generateTokens.js";
+import { generateAccessToken, generateRefreshToken } from "../utils/generateTokens.js";
 
 export const signupUser = async (req, res) => {
     const { name, email, password } = req.body;
@@ -30,13 +30,13 @@ export const signupUser = async (req, res) => {
 
         await newUser.save();
 
-        res.status(201).json({
+        return res.status(201).json({
             message: "User created successfully."
         })
 
     } catch (error) {
         console.log(error);
-        res.status(500).json({
+        return res.status(500).json({
             message: "Something went wrong."
         });
     }
@@ -52,7 +52,7 @@ export const loginUser = async (req, res) => {
             });
         }
 
-        const currentUser = await User.findOne({ email });
+        const currentUser = await User.findOne({ email: email.toLowerCase() });
         
         if(!currentUser) {
             return res.status(400).json({
@@ -72,16 +72,249 @@ export const loginUser = async (req, res) => {
 
         const refreshToken = generateRefreshToken(currentUser._id, rememberMe);
 
+        currentUser.refreshToken = refreshToken;
+        await currentUser.save();
+
         res.status(200).json({
             message: "Login successful.",
             refreshToken,
-            accessToken
+            accessToken,
+            user: {
+                id: currentUser._id,
+                name: currentUser.name,
+                email: currentUser.email
+            }
         });
     } catch (error) {
         console.log(error);
-        res.status(500).json({
+        return res.status(500).json({
             message: "Something went wrong."
         });
     }
 };
 
+export const refreshAccessToken = async (req, res) => {
+    try {
+        const { refreshToken } = req.body;
+
+        if(!refreshToken) {
+            return res.status(400).json({
+                message: "Refresh token required."
+            });
+        }
+
+        const decodedUser = jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET);
+        
+        const currentUser = await User.findById(decodedUser.id);
+        if(!currentUser) {
+            return res.status(404).json({
+                message: "User not found."
+            })
+        }
+
+        if(currentUser.refreshToken !== refreshToken) {
+            return res.status(403).json({
+                message: 'Invalid refresh token.'
+            });
+        }
+
+        const accessToken = generateAccessToken(currentUser._id);
+        return res.status(200).json({
+            accessToken
+        })
+    } catch (error) {
+        if(error.name === "JsonWebTokenError"){
+            return res.status(403).json({
+                message: "Invalid refresh token."
+            });
+        }
+        if(error.name==="TokenExpiredError") {
+            return res.status(403).json({
+                message: "Refresh token expired."
+            });
+        }
+        console.log(error);
+        return res.status(500).json({
+            message: "Something went wrong."
+        });
+    }
+}
+
+export const logoutUser = async (req, res) => {
+    try {
+        const currentUser = await User.findByIdAndUpdate(req.user.id,
+            { refreshToken: null },
+        );
+
+        if(!currentUser) {
+            return res.status(404).json({
+                message: "User not found."
+            })
+        }
+
+        return res.status(200).json({
+            message: "Logout successful."
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message: "Something went wrong."
+        });
+    }
+}
+
+export const deleteAccount = async (req, res) => {
+    try {
+        const currentUser = await User.findByIdAndDelete(req.user.id);
+        if(!currentUser) {
+            return res.status(404).json({
+                message: "User not found."
+            });
+        }
+        return res.status(200).json({
+            message: "Account deleted successfully."
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message: "Something went wrong."
+        });
+    }
+};
+
+export const getCurrentUser = async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.user.id);
+        if(!currentUser) {
+            return res.status(404).json({
+                message: "User not found."
+            });
+        }
+        return res.status(200).json({
+            user: {
+                id: currentUser._id,
+                name: currentUser.name,
+                email: currentUser.email,
+                profilePicture: currentUser.profilePicture,
+                savedPlaces: currentUser.savedPlaces,
+                recentSearches: currentUser.recentSearches
+            }
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message: "Something went wrong."
+        });
+    }
+};
+
+export const updateProfile = async (req, res) => {
+    try {
+        const { name, email } = req.body;
+
+        const currentUser = await User.findById(req.user.id);
+
+        if (!currentUser) {
+            return res.status(404).json({
+                message: "User not found."
+            });
+        }
+
+        const trimmedName = name?.trim();
+        const trimmedEmail = email?.trim();
+
+        if (trimmedEmail) {
+            const existingUser = await User.findOne({
+                email: trimmedEmail
+            });
+
+            if (
+                existingUser &&
+                !existingUser._id.equals(currentUser._id)
+            ) {
+                return res.status(400).json({
+                    message: "Email already exists."
+                });
+            }
+        }
+
+        if (trimmedName) {
+            currentUser.name = trimmedName;
+        }
+
+        if (trimmedEmail) {
+            currentUser.email = trimmedEmail;
+        }
+
+        await currentUser.save();
+
+        return res.status(200).json({
+            message: "Profile updated successfully.",
+            user: {
+                id: currentUser._id,
+                name: currentUser.name,
+                email: currentUser.email,
+                profilePicture: currentUser.profilePicture,
+                savedPlaces: currentUser.savedPlaces,
+                recentSearches: currentUser.recentSearches
+            }
+        });
+
+    } catch (error) {
+        console.log(error);
+
+        return res.status(500).json({
+            message: "Something went wrong."
+        });
+    }
+};
+
+export const changePassword = async (req, res) => {
+    try {
+        const { oldPassword, newPassword } = req.body;
+
+        if(!oldPassword || !newPassword) {
+            return res.status(400).json({
+                message: "Old password and new password are required."
+            })
+        }
+
+        const currentUser = await User.findById(req.user.id);
+
+        if(!currentUser) {
+            return res.status(404).json({
+                message: "User not found."
+            })
+        }
+
+        if (oldPassword === newPassword) {
+          return res.status(400).json({
+          message:
+          "New password must be different from the old password."
+         });
+        }
+
+        const isPasswordValid = await bcrypt.compare(oldPassword, currentUser.password);
+
+        if(!isPasswordValid) {
+            return res.status(400).json({
+                message: "Old password is incorrect."
+            })
+        }
+
+        const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+        currentUser.password = hashedPassword;
+        currentUser.refreshToken = null;
+        await currentUser.save();
+
+        return res.status(200).json({
+            message: "Password changed successfully."
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message: "Something went wrong."
+        });
+    }
+}
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,10 +1,13 @@
 import express from "express";
 
-import { signupUser, loginUser } from "../controllers/authController.js";
+import { signupUser, loginUser, logoutUser, deleteAccount } from "../controllers/authController.js";
+import authMiddleware from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/signup", signupUser);
 router.post("/login", loginUser);
+router.post("/logout",authMiddleware, logoutUser);
+router.delete("/delete",authMiddleware, deleteAccount);
 
 export default router;

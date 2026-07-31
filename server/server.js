28b321b001ea35b3b37b088f9dedd2008abf1d10
Twisha Patel <twipatel2006@gmail.com>
2026-07-31 16:42:08 +0530

    Update

--- server/server.js
+++ server/server.js
@@ -7,39 +7,56 @@ import { connectDB } from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 
-
 dotenv.config();
 
 const app = express();
 
-
 app.use(
+
     cors({
-        origin: "http://localhost:5173",
+
+        origin: [
+
+            "http://localhost:5173",
+
+            process.env.CLIENT_URL,
+
+        ],
+
+        credentials: true,
+
     })
+
 );
 
 
 app.use(express.json());
 
-
 connectDB();
-
 
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 
 
 app.get("/", (req, res) => {
-    res.send("MediLink Backend is running.");
+
+    res.send(
+        "MediLink Backend is running."
+    );
+
 });
 
 
-const PORT = process.env.PORT || 5000;
+const PORT =
+    process.env.PORT || 5000;
 
 
 app.listen(PORT, () => {
+
     console.log(
+
         `Server is running on ${PORT}`
+
     );
+
 });
